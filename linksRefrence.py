01a79f55abfe50666646800f3b649553acced1cb
Sudais Khan <sudais090802@gmail.com>
2026-06-24 22:55:58 +0500

    jkl

---
+++ linksRefrence.py
@@ -0,0 +1,10 @@
+# Logo
+#https://raw.githubusercontent.com/sadais65001/goldbaseMerchants/main/{merchantId}/{logoExactName.formate}
+
+# shopPhotos
+#https://raw.githubusercontent.com/sadais65001/goldbaseMerchants/main/{merchantId}/shopPhotos/{shopPhotoExactName.formate}
+
+# util
+#TikTok
+#https://raw.githubusercontent.com/sadais65001/goldbaseMerchants/main/utils/{exactPhotoName.formate}
+
